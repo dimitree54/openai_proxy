c68c8240141c337b60a81ce4270ea81131b8a19b
Dmitrii Rashchenko <d.rashchenko@planner5d.com>
2023-10-01 10:05:18 +0300

    rate limiting

--- server.py
+++ server.py
@@ -2,6 +2,8 @@ import tempfile
 
 import openai
 from flask import Flask, request, jsonify
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 
 
 def generate_corrected_transcript(text: str) -> str:
@@ -36,24 +38,33 @@ def recognise_speech(m4a_file_path, smart_mode: bool):
 
 
 app = Flask(__name__)
+limiter = Limiter(
+    get_remote_address,
+    app=app,
+    default_limits=["200 per day", "50 per hour"],
+    storage_uri="memory://"
+)
 
 
 @app.route('/recognise', methods=['POST'])
+@limiter.limit("10 per minute")
 def recognise_endpoint():
     if 'm4a_file' not in request.files or 'smart_mode' not in request.form:
+        print("error missing m4a_file or smart_mode parameter")
         return jsonify({'error': 'Missing m4a_file or smart_mode parameter'}), 400
 
-    m4a_file = request.files['m4a_file']
-    print(f"File size: {len(m4a_file.read())} bytes")
-    m4a_file.seek(0)
-
-    smart_mode = request.form['smart_mode'].lower() == 'true'
-    temp_file = tempfile.NamedTemporaryFile(delete=False, suffix=".m4a")
-    m4a_file.save(temp_file.name)
-    temp_file.flush()
-    transcript = recognise_speech(temp_file.name, smart_mode)
-    temp_file.close()
-
+    try:
+        m4a_file = request.files['m4a_file']
+        smart_mode = request.form['smart_mode'].lower() == 'true'
+        temp_file = tempfile.NamedTemporaryFile(delete=False, suffix=".m4a")
+        m4a_file.save(temp_file.name)
+        temp_file.flush()
+        transcript = recognise_speech(temp_file.name, smart_mode)
+        temp_file.close()
+    except Exception as e:
+        print(f"error {str(e)}")
+        return jsonify({'error': 'Error during recognition.'}), 400
+    print("Successful recognition")
     return jsonify({'transcript': transcript})
 
 
