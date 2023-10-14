e83e0d3656b4d8bf556a61d9270f67d1c368c851
Dmitrii Rashchenko <dimitree54@gmail.com>
2023-10-14 12:59:31 +0300

    edit endpoint

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ from flask_limiter.util import get_remote_address
 from typing import Tuple
 
 
-class TranscriptGenerator:
+class SmartEditor:
     def __init__(self):
         self.model = "gpt-4"
         self.temperature = 0
@@ -33,11 +33,35 @@ The next message from user is a result of voice recognition. It contain text use
         )
         return response['choices'][0]['message']['content']
 
+    def edit(self, text2edit: str, commands: str) -> str:
+        response = openai.ChatCompletion.create(
+            model=self.model,
+            temperature=self.temperature,
+            messages=[
+                {
+                    "role": "system",
+                    "content": """You are an AI tool for editing text.
+At first user will send you a text to edit and then commands of how to edit that text.
+You need to apply requested modifications and answer with edited text.
+Do not include any comments from you, only answer with edited text."""
+                },
+                {
+                    "role": "user",
+                    "content": text2edit
+                },
+                {
+                    "role": "user",
+                    "content": commands
+                }
+            ]
+        )
+        return response['choices'][0]['message']['content']
+
 
 class SpeechRecognizer:
     def __init__(self, smart_mode: bool):
         self.smart_mode = smart_mode
-        self.transcript_generator = TranscriptGenerator()
+        self.transcript_generator = SmartEditor()
 
     def recognise_speech(self, m4a_file_path: str) -> str:
         with open(m4a_file_path, "rb") as audio_file:
@@ -75,6 +99,29 @@ def recognise_endpoint() -> Tuple[str, int]:
         return jsonify({'error': 'Error during recognition.'}), 400
     app.logger.info("Successful recognition")
     return jsonify({'transcript': transcript})
+
+
+@app.route('/edit', methods=['POST'])
+@limiter.limit("10 per minute")
+def edit_endpoint() -> Tuple[str, int]:
+    if 'm4a_file' not in request.files or 'text' not in request.form:
+        app.logger.error("Missing m4a_file or text parameter")
+        return jsonify({'error': 'Missing m4a_file or text parameter'}), 400
+    try:
+        m4a_file = request.files['m4a_file']
+        text = request.form['text']
+        with tempfile.NamedTemporaryFile(delete=True, suffix=".m4a") as temp_file:
+            m4a_file.save(temp_file.name)
+            temp_file.flush()
+            recognizer = SpeechRecognizer(False)
+            voice_commands = recognizer.recognise_speech(temp_file.name)
+            transcript_generator = SmartEditor()
+            edited_text = transcript_generator.edit(text, voice_commands)
+    except Exception as e:
+        app.logger.error(f"Error during editing: {str(e)}")
+        return jsonify({'error': 'Error during editing.'}), 400
+    app.logger.info("Successful editing")
+    return jsonify({'edited_text': edited_text})
 
 
 if __name__ == '__main__':
